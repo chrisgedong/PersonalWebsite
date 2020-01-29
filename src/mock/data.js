36d862dc4fcbdc7b99cb52c52a196899514c8249
PotatoCoder3000 <31225100+PotatoCoder3000@users.noreply.github.com>
2020-01-29 13:54:05 -0800

    Aesthetic Modifications

--- src/mock/data.js
+++ src/mock/data.js
@@ -90,5 +90,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
