88746ec62462a660c3c136bd43d3414ffcfff222
PotatoCoder3000 <31225100+PotatoCoder3000@users.noreply.github.com>
2020-01-29 14:06:49 -0800

    Cleaning up

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -28,7 +28,7 @@ const Projects = () => {
         <div className="project-wrapper">
           <Title title="Projects" />
           {projects.map(project => {
-            const { id, title, info, info2, url, repo, img } = project;
+            const { id, title, info, info2, url, img } = project;
 
             return (
               <Row key={id}>
@@ -49,25 +49,6 @@ const Projects = () => {
                         </p>
                         <p className="mb-4">{info2 || ''}</p>
                       </div>
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="cta-btn cta-btn--hero"
-                        href={url || '#!'}
-                      >
-                        See Live
-                      </a>
-
-                      {repo && (
-                        <a
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="cta-btn text-color-main"
-                          href={repo}
-                        >
-                          Source Code
-                        </a>
-                      )}
                     </div>
                   </Fade>
                 </Col>
