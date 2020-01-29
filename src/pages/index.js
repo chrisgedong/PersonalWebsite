42b25bfb371117f6957adb7fcb825888189d224b
PotatoCoder3000 <31225100+PotatoCoder3000@users.noreply.github.com>
2020-01-29 13:32:45 -0800

    Changing tile

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Chris' Website}</title>
+        <title>{title || 'Chris''s Website}</title>
         <html lang={lang || 'en'} />
         <meta name="description" content={description || 'Gatsby Simplefolio'} />
       </Helmet>
