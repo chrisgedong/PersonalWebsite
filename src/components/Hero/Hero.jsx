5166d65ac79a5cfb9766f6676ff53dcc8bfd3518
PotatoCoder3000 <31225100+PotatoCoder3000@users.noreply.github.com>
2020-01-29 13:24:51 -0800

    Adding name

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,9 +26,9 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || 'Chris Dong'}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || "The Software Developer."}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
