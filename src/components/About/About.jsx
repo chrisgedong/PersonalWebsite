479ff882f0d4cff35fc0e3b33f57ee5c1e068c37
PotatoCoder3000 <31225100+PotatoCoder3000@users.noreply.github.com>
2020-01-29 15:41:41 -0800

    Adding to static folder

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -54,7 +54,7 @@ const About = () => {
                       target="_blank"
                       rel="noopener noreferrer"
                       className="cta-btn cta-btn--resume"
-                      href="../../../public/resume/resume.pdf"
+                      href="../../../public/static/resume/resume.pdf"
                     >
                       Resume
                     </a>
