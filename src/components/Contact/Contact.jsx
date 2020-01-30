e755c629dd8c45f4cc86ab853bc2e07403ed1967
PotatoCoder3000 <31225100+PotatoCoder3000@users.noreply.github.com>
2020-01-29 16:36:10 -0800

    Trying something new

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,9 +15,13 @@ const Contact = () => {
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
             <p className="contact-wrapper__text">
-              {cta ||
-                'Would you like to work with me? Awesome! <br>Email: chrisdong916@gmail.com <br>Phone: +1-(916)-670-4834'}
+              {cta || 'Would you like to work with me? Awesome!'}
             </p>
+            <p className="contact-wrapper__text">
+              {cta || 'Email: chrisdong916@gmail.com'}
+            </p>
+            <p className="contact-wrapper__text">
+              {cta || 'Phone: +1-(916)-670-4834'}</p>
           </div>
         </Fade>
       </Container>
